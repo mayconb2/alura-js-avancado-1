7d1cdf8b32c4cb994da652f190ff0fa38130091d
mayconb2 <mayconb2@gmail.com>
2019-11-27 20:44:55 -0300

    Criado classe ListaNegociacao que servirá o controleler para mostrar lista de ngociacoes

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,11 +1,12 @@
 class NegociacaoController {
-
+    
     constructor() {
-
+        
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();    
 
     }
 
@@ -13,14 +14,25 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _limpaFormulario() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    _criaNegociacao () {
+
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        );
-
-        console.log(negociacao);
-        console.log(DateHelper.textoParaData(this._inputData.value))
-        console.log(DateHelper.dataParaTexto(negociacao.data))
+            this._inputValor.value);
     }
 }
